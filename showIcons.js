e4dc2092cff340cc60f7fca897bbfdedde6816de
n-nakajm <n-nakajm@ist.osaka-u.ac.jp>
2020-08-29 23:54:38 +0900

    fix bug

--- showIcons.js
+++ showIcons.js
@@ -1,22 +1,20 @@
 const setLeftPosition = (iconWidth) => {
     const browserWidth = document.getElementById("main-wrapper").clientWidth
-    return ((Math.random() * (browserWidth - iconWidth)) + "px")
+    const position = Math.random() * (browserWidth - iconWidth)
+    return (position + "px")
 }
 
 const setTopPosition = (iconHeight) => {
     const browserHeight = document.getElementById("main-wrapper").clientHeight
     const headerHeight = document.getElementById("header-wrapper").clientHeight
-    let position = Math.random() * (browserHeight - iconHeight) + headerHeight
-    const footerPosition = document.getElementById("footer-wrapper").getBoundingClientRect().top
-    if (headerHeight + position + iconHeight > footerPosition) {
-        position = position - headerHeight
-    }
+    let position = headerHeight + Math.random() * (browserHeight - iconHeight - headerHeight)
     return (position + "px")
 }
 
 const appier = () => {
     let img = document.createElement("img")
-    img.src = "./icon/ouen-13.png"
+    let rand = Math.floor(Math.random() * 20) + 1; //乱数を発生;
+    img.src = "./icon/ouen-" + rand + ".png"
     img.classList.add("icon")
 
     img.style.position = "absolute"
