9c3e60546e80f924f02a5bb8a189b8b0dbe2f20c
n-nakajm <n-nakajm@ist.osaka-u.ac.jp>
2020-08-30 11:09:10 +0900

    refactor + change volume

--- socket.js
+++ socket.js
@@ -4,28 +4,31 @@ const max_main = 8
 window.onload = () => {
     const host = "wss://websockets-server-test.herokuapp.com/pipe";
     //var host = "ws://localhost:8080/pipe";
-    var ws = new WebSocket(host);
+    const ws = new WebSocket(host);
 
-    document.getElementById("btn").addEventListener("click", () => {
+    const forDebug = () => {
         const message = "Debug Message"
         ws.send("{ \"id\":\"client\", \"text\":\""+message+"\"}")
-    })
+    }
 
-    ws.onmessage = (message) => {
-
+    const todokeyell = () => {
         appear()
 
         const sub_rand = Math.floor(Math.random() * max_sub) + 1 //乱数を発生
         const sub_mp3path = "./sound/call-sub-" + sub_rand + ".mp3"
         const subcall = new Audio(sub_mp3path)
+        subcall.volume = 0.4
         subcall.play()  // 再生
         subcall.currentTime = 0
 
         const main_rand = Math.floor(Math.random() * max_main) + 1 //乱数を発生
         const main_mp3path = "./sound/call-main-" + main_rand + ".mp3"
         const maincall = new Audio(main_mp3path)
+        maincall.volume = 0.7
         maincall.play()  // 再生
         maincall.currentTime = 0
-
     }
+
+    document.getElementById("btn").addEventListener("click", forDebug)
+    ws.addEventListener("message", todokeyell)
 }
